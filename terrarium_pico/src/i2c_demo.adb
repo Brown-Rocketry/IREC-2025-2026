951786f37b72707dbd409d43bd4ba0c0012cd320
MargXX <margeauxcorrigan@gmail.com>
2026-03-25 01:14:16 -0400

    gyro data working

--- terrarium_pico/src/i2c_demo.adb
+++ terrarium_pico/src/i2c_demo.adb
@@ -23,17 +23,24 @@ procedure i2c_demo is
    Addr_AG  : constant HAL.I2C.I2C_Address := 16#D6#;  -- 0x6B shifted left = 0xD6
    Addr_Mag : constant HAL.I2C.I2C_Address := 16#3C#;  -- 0x1E shifted left = 0x3C
 
-   REG_WHO_AM_I : constant UInt8 := 16#0F#;
-   --  AG  WHO_AM_I expected: 0x68
-   --  Mag WHO_AM_I expected: 0x3D
-   CTRL_REG6_XL : constant UInt8 := 16#20#;
-   OUT_X_L_XL : constant UInt8 := 16#28#;
+   --for accelerometer
+   CTRL_REG6_XL : constant UInt8 := 16#20#; -- enable register
+   OUT_X_L_XL : constant UInt8 := 16#28#; -- data register
    -- read 6 bytes from this, 2 bytes for x y and z
+   
+   --for gyroscope
+   CTRL_REG1_G : constant UInt8 := 16#10#; --enable register
+   OUT_X_L_G : constant UInt8 := 16#18#; --data register
+   --same thing with 2 bytes per axis
 
-   ODR_XL : constant UInt8 := 16#60#;
+
+   ODR_XL : constant UInt8 := 16#60#; --personal settings  for accelerometer
    -- bits: 0 1 1  0 0  0  0 0
-   -- ODR=011 FS=00 BW_SCAL=0 BW=00
-   -- values scale to +- 2g at max and min
+   -- ODR=011 FS=00 Required 0=0 BW=00
+
+   ODR_G : constant UInt8 := 16#68#; --personal settings  for gyro
+   -- bits: 0 1 1  0 1  0  0 0
+   -- ODR=011 FS=01 BW_SCAL=0 BW=00
 
    procedure Put_Line (S : String) is
       Bytes : UART_Data_8b (1 .. S'Length + 2);
@@ -51,6 +58,7 @@ procedure i2c_demo is
 
    procedure Read_Data
       (Label    : String;
+       Mem_Addr : UInt8;
        Addr     : HAL.I2C.I2C_Address)
    is
       Port   : RP.I2C_Master.I2C_Master_Port renames RP.Device.I2CM_0;
@@ -61,7 +69,7 @@ procedure i2c_demo is
       Put_Line ("Reading Data for " & Label);
       Port.Mem_Read
         (Addr          => Addr,
-         Mem_Addr      => UInt16 (OUT_X_L_XL),
+         Mem_Addr      => UInt16 (Mem_Addr),
          Mem_Addr_Size => Memory_Size_8b,
          Data          => Data,
          Status        => Stat,
@@ -72,9 +80,9 @@ procedure i2c_demo is
          for Coord in 1 .. 3 loop
             -- low first then high?
             declare
-               Lo  : Unsigned_16 := Unsigned_16(Data(2*Coord - 1));
-               Hi  : Unsigned_16 := Unsigned_16(Data(2*Coord));
-               U   : Unsigned_16 := Hi * 256 + Lo;
+               Lo  : constant Unsigned_16 := Unsigned_16(Data(2*Coord - 1));
+               Hi  : constant Unsigned_16 := Unsigned_16(Data(2*Coord));
+               U   : constant Unsigned_16 := Hi * 256 + Lo;
                S   : Integer_32  := Integer_32(U);
             begin
                -- trick giving a sign to correct for the unsigned version, using this trick because converting an integer to string causes a hanging
@@ -95,6 +103,7 @@ procedure i2c_demo is
    procedure Enable_Sensor
      (Label    : String;
       Addr     : HAL.I2C.I2C_Address; 
+      Mem_Addr : UInt8;
       Code     : UInt8)
    is
       Port   : RP.I2C_Master.I2C_Master_Port renames RP.Device.I2CM_0;
@@ -104,7 +113,7 @@ procedure i2c_demo is
       Put_Line ("Enable for " & Label);
       Port.Mem_Write
            (Addr          => Addr,
-            Mem_Addr      => UInt16 (CTRL_REG6_XL),
+            Mem_Addr      => UInt16 (Mem_Addr),
             Mem_Addr_Size => Memory_Size_8b,
             Data          => Data,
             Status        => Stat,
@@ -138,11 +147,13 @@ begin
    
    Put_Line ("Starting..."); 
 
-   Enable_Sensor ("LSM9DS1 AG", Addr_AG, ODR_XL);
+   Enable_Sensor ("LSM9DS1 Accel", Addr_AG, CTRL_REG6_XL, ODR_XL); --accel
+   Enable_Sensor ("LSM9DS1 Gyro", Addr_AG, CTRL_REG1_G, ODR_G); --gyro
 
    loop
       Put_Line ("loop");  
-      Read_Data ("LSM9DS1 AG", Addr_AG);
+      Read_Data ("LSM9DS1 AG", OUT_X_L_XL, Addr_AG); --accel
+      Read_Data ("LSM9DS1 AG", OUT_X_L_G, Addr_AG); --gyro
       RP.Device.Timer.Delay_Milliseconds (3000);
       Pico.LED.Toggle;
    end loop;
